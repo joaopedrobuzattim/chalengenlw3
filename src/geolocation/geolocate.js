d26a60eaad6aeb6fd0ba70a09046b978805ce752
João Pedro Buzatti Mendes <71409819+joaopedrobuzattim@users.noreply.github.com>
2020-10-19 01:17:06 -0300

    Refactoring

--- src/geolocation/geolocate.js
+++ src/geolocation/geolocate.js
@@ -1,33 +1,23 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = function (address) {
+  const parsedAddress = address
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/ /g, "+");
 
-    const parsedAddress = address.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/ /g, "+");
+  const coordenates = fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${parsedAddress}&key=INSERT_YOUR_API_KEY_HERE`
+  )
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      let geolocationObject = data.results[0].geometry.location;
+      const { lat, lng } = geolocationObject;
+      const latLngFocus = { lat, lng };
+      return latLngFocus;
+    });
 
-    const url = fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${parsedAddress}&key=AIzaSyDk4HWqPCuAR-8JxW09zKOuvhFAZHhA2s0`
-      )
-      .then((result)=>{
-            return result.json()
-      })
-      .then((data)=>{
-            let geolocationObject = data.results[0].geometry.location;
-            const { lat, lng } = geolocationObject;
-            const latLngFocus = [{ lat, lng }];
-            return latLngFocus
-      })
-
-      return new Promise((resolve, reject)=>{
-           
-        if(!url){
-            reject("Invalid input!")
-        }
-
-        resolve (url);
-
-      })
-
-
-}
-
-
+  return coordenates;
+};
